f55d7a017e76773fcb708b7e9ccf0dedbb7e4903
Bashu23 <abhandari@mail.walshcollege.edu>
2024-08-26 13:05:44 -0400

    edits

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -9,7 +9,7 @@ export const postConfirmation = defineFunction({
 // /Users/ashwinibhandari/profilesapp/amplify/data/resource.ts
 
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { postConfirmation } from "/Users/ashwinibhandari/profilesapp/amplify/auth/post-confirmation/resource.ts";  // Full path to the resource file
+import { postConfirmation } from "../auth/post-confirmation/resource"; // Correct relative path
 
 const schema = a
   .schema({
@@ -35,4 +35,3 @@ export const data = defineData({
     },
   },
 });
-
